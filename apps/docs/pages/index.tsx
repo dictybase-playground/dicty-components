afa6932fe0bf278f56e81ce7034516bc7fbdab7d
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-01-25 15:37:48 -0600

    build: add navbar package

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,10 +1,11 @@
-import { Button } from "ui";
+import { Navbar } from "navbar";
 
 export default function Docs() {
   return (
     <div>
       <h1>Docs</h1>
-      <Button />
+
+      <Navbar />
     </div>
   );
 }
