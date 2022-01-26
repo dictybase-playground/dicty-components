1a543a1c8d7e60e6186d63e645b18b2bc487ab4c
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-01-25 18:36:23 -0600

    feat: add navbar data

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,11 +1,12 @@
 import { Navbar } from "navbar";
+import { data } from "./navbar-items";
 
 export default function Docs() {
   return (
     <div>
       <h1>Docs</h1>
 
-      <Navbar />
+      <Navbar items={data} />
     </div>
   );
 }
