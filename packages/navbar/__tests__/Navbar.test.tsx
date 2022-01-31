433f3cf512c9538d1922b9835ac47fe1fbf415a4
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-01-31 09:57:01 -0600

    test: add unit tests with dropdown click events

--- packages/navbar/__tests__/Navbar.test.tsx
+++ packages/navbar/__tests__/Navbar.test.tsx
@@ -1,29 +1,34 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import React from "react"
 import { Navbar } from "../src/components/Navbar"
 
+const navData1 = [
+  {
+    type: "link1",
+    id: "1",
+    attributes: {
+      display: "Link 1",
+      items: [
+        {
+          label: "Sublink 1",
+          link: "/sublink1",
+        },
+      ],
+    },
+  },
+]
+
 describe("src/components/Navbar", () => {
   it("should render navbar with 1 link, and 1 sublink", () => {
-    render(
-      <Navbar
-        items={[
-          {
-            type: "link1",
-            id: "1",
-            attributes: {
-              display: "Link 1",
-              items: [
-                {
-                  label: "Sublink 1",
-                  link: "/sublink1",
-                },
-              ],
-            },
-          },
-        ]}
-      />,
-    )
-    expect(screen.getByText("Link 1")).toBeInTheDocument()
-    expect(screen.getByText("Sublink 1")).toBeInTheDocument()
+    render(<Navbar items={navData1} />)
+
+    const item1 = navData1[0].attributes
+    const link1 = screen.getByText(item1.display)
+    expect(link1).not.toBeNull()
+
+    // Click on link 1 to open up dropdown with sub link 1
+    userEvent.click(link1)
+    expect(screen.getByText("Sublink 1")).not.toBeNull()
   })
 })
