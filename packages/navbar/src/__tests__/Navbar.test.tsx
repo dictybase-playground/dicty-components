457a24ed0e3c062742df4f025e969e4ae78c34b3
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-01-31 13:16:17 -0600

    refactor: move jest tests to src/__tests__

--- packages/navbar/src/__tests__/Navbar.test.tsx
+++ packages/navbar/src/__tests__/Navbar.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
-import React from "react"
 import { Navbar } from "navbar"
 import { navData1 } from "navbar/src/data/testData"
 
