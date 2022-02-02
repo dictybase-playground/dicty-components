0fe41b30c905143c5c63dc823dc8bce2aa0e65d4
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-02-02 16:52:19 -0600

    build: add coverage ignore + test path ignore

--- packages/navbar/jest.config.js
+++ packages/navbar/jest.config.js
@@ -7,4 +7,12 @@ module.exports = {
   },
   testEnvironment: "jsdom",
   preset: "ts-jest",
+  testPathIgnorePatterns: ["src/e2e/"],
+  coveragePathIgnorePatterns: [
+    "dist/",
+    "src/data/",
+    "src/types/",
+    "src/e2e/",
+    "src/styles/",
+  ],
 }
