1660ec04250b98d62e749d1a2a9b2adde7dae6d9
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-02-09 15:11:06 -0600

    feat: add more header types

--- packages/header/src/types/index.ts
+++ packages/header/src/types/index.ts
@@ -3,12 +3,20 @@ import { Theme } from "@material-ui/core"
 export interface HeaderProps {
   /** List of header items (links) */
   items: HeaderItem[]
-  /** Link for the logo that goes to the home page */
   home?: string
   /** Render function to display list of links in header */
   render: ({ items }: { items: HeaderItem[] }) => JSX.Element
-  /** CSS color theme to use in header */
-  theme: Theme
+  theme?: Partial<Theme>
+  logo: string
 }
 
 export interface HeaderItem {}
+
+export interface HeaderContainerProps {
+  /** List of header items (links) */
+  items: HeaderItem[]
+  home?: string
+  /** Render function to display list of links in header */
+  render: ({ items }: { items: HeaderItem[] }) => JSX.Element
+  logo: string
+}
