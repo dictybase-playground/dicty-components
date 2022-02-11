877517f28506819fcc63f42df9c9966932de5549
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-02-11 09:32:23 -0600

    test: add test cases for header with links rendered

--- packages/header/src/__tests__/Header.test.tsx
+++ packages/header/src/__tests__/Header.test.tsx
@@ -7,4 +7,23 @@ describe("src/components/Header", () => {
 
     expect(screen.getByText("Dicty Community Resource")).not.toBeNull()
   })
+
+  it("should render header with links", () => {
+    render(
+      <Header
+        links={[
+          {
+            url: "/foo",
+            text: "Link",
+            icon: <svg />,
+            isNextLink: false,
+          },
+        ]}
+        logo={""}
+      />,
+    )
+
+    expect(screen.getByText("Dicty Community Resource")).not.toBeNull()
+    expect(screen.getByText("Link")).not.toBeNull()
+  })
 })
