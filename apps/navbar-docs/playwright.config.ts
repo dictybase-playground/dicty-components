89aa3c09f36ae82923332117147702fa264a0341
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-02-11 09:35:18 -0600

    build: use playwright server startup port from package.json

--- apps/navbar-docs/playwright.config.ts
+++ apps/navbar-docs/playwright.config.ts
@@ -1,10 +1,11 @@
 import { PlaywrightTestConfig } from "@playwright/test"
+import { port } from "./package.json"
 
 const config: PlaywrightTestConfig = {
   testDir: "./e2e",
   webServer: {
     command: "yarn dev",
-    port: 3001,
+    port: parseInt(port),
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
