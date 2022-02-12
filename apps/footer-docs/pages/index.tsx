216a5cf009660a8f053a39c624494bb848c8df47
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-02-11 19:12:06 -0600

    feat: add footer data + links

--- apps/footer-docs/pages/index.tsx
+++ apps/footer-docs/pages/index.tsx
@@ -3,7 +3,46 @@ import { Footer } from "footer"
 export default function Docs() {
   return (
     <div>
-      <Footer links={[]} />
+      <Footer
+        links={[
+          {
+            url: "/research/techniques",
+            description: "Techniques",
+          },
+          {
+            url: "/research/teach",
+            description: "Teaching Protocols",
+          },
+          {
+            url: "/stockcenter",
+            description: "Dicty Stock Center",
+          },
+          {
+            url: "http://dictybase.org/tools/jbrowse/?data=data%2Fjbrowse%2Fdiscoideum&loc=6%3A1..50022&tracks=reference%2Cgene%2Ctranscript&highlight=",
+            description: "Genome Browser",
+          },
+          {
+            url: "/dictyaccess",
+            description: "DictyAccess",
+          },
+          {
+            url: "/community/conference",
+            description: "Conference",
+          },
+          {
+            url: "/community/labs",
+            description: "Labs",
+          },
+          {
+            url: "/about",
+            description: "About",
+          },
+          {
+            url: "/stockcenter/contact",
+            description: "Contact",
+          },
+        ]}
+      />
     </div>
   )
 }
