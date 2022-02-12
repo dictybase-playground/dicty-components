0256e34c9bacaf5fcdee71977134403997ce7842
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-02-12 10:07:27 -0600

    feat: export footer data and styles

--- apps/footer-docs/pages/index.tsx
+++ apps/footer-docs/pages/index.tsx
@@ -1,48 +1,9 @@
-import { Footer } from "footer"
+import { Footer, footerData } from "footer"
 
 export default function Docs() {
   return (
     <div>
-      <Footer
-        links={[
-          {
-            url: "/research/techniques",
-            description: "Techniques",
-          },
-          {
-            url: "/research/teach",
-            description: "Teaching Protocols",
-          },
-          {
-            url: "/stockcenter",
-            description: "Dicty Stock Center",
-          },
-          {
-            url: "http://dictybase.org/tools/jbrowse/?data=data%2Fjbrowse%2Fdiscoideum&loc=6%3A1..50022&tracks=reference%2Cgene%2Ctranscript&highlight=",
-            description: "Genome Browser",
-          },
-          {
-            url: "/dictyaccess",
-            description: "DictyAccess",
-          },
-          {
-            url: "/community/conference",
-            description: "Conference",
-          },
-          {
-            url: "/community/labs",
-            description: "Labs",
-          },
-          {
-            url: "/about",
-            description: "About",
-          },
-          {
-            url: "/stockcenter/contact",
-            description: "Contact",
-          },
-        ]}
-      />
+      <Footer links={footerData} />
     </div>
   )
 }
