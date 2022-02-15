28ff1bc027b2b98fb75fbdfb18accf968110f06e
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-02-14 18:37:35 -0600

    build: upgrade playwright to v1.19.0

--- apps/header-docs/e2e/header.spec.ts
+++ apps/header-docs/e2e/header.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test"
 import { port } from "../package.json"
 
 test("header browser e2e test", async ({ page }) => {
-  await page.goto(`http://localhost:${port}`, { timeout: 0 })
+  await page.goto(`http://localhost:${port}`)
 
   const genomesOp = page.locator("text=Dicty Community Resource")
   await expect(genomesOp).toContainText("Dicty Community Resource")
