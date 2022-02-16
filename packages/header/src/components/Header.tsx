147ef4326914267c4e22b74d9572066394719359
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-02-16 15:11:15 -0600

    docs: add tsdoc component usage example

--- packages/header/src/components/Header.tsx
+++ packages/header/src/components/Header.tsx
@@ -3,6 +3,30 @@ import { HeaderProps } from "header/src/types"
 import { muiTheme } from "navbar/src/styles/customTheme"
 import { HeaderContainer } from "header/src/components/HeaderContainer"
 
+/**
+ * Renders dictyBase header
+ * 
+ * ```tsx
+ * import { Header, HeaderLink } from "header"
+ * import logo from "header/src/images/logo.png"
+ * 
+ * <Header logo={logo.src} caption="Dicty Community Resource">
+ *  <HeaderLink 
+ *    url="/foo" 
+ *    text="Foo" 
+ *    icon={<AddIcon fontSize="large" />}
+ *    isNextLink
+ *  />
+ *  ...
+ *  <HeaderLink 
+ *    url="/bar" 
+ *    text="Bar" 
+ *    icon={<svg />}
+ *    isNextLink
+ *  />
+ * </Header>
+ * ```
+ */
 export const Header = ({ theme, children, ...rest }: HeaderProps) => {
   const customTheme = theme ? theme : muiTheme
 
