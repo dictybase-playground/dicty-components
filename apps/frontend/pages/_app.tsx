ab3c7a0b342290f59813075f7bb413bd2e4fc34d
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-02-28 16:42:37 -0600

    feat: add header, navbar, and footer to main _app

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -2,6 +2,14 @@ import { createTheme, MuiThemeProvider, CssBaseline } from "@material-ui/core"
 import { AppProps } from "next/app"
 import Head from "next/head"
 import ClientOnly from "../components/clientOnly"
+import { Header, HeaderLink, HeaderLinkProps } from "@dictyBase/header"
+import logo from "../public/logo.png"
+import AddIcon from "@material-ui/icons/Add"
+import FileDownloadIcon from "@material-ui/icons/GetApp"
+import InfoIcon from "@material-ui/icons/Info"
+import LoginIcon from "@mui/icons-material/Login"
+import { Navbar, navbarData } from "@dictyBase/navbar"
+import { Footer, FooterLink, footerData } from "@dictyBase/footer"
 
 const muiTheme = createTheme({
   palette: {
@@ -60,6 +68,33 @@ const muiTheme = createTheme({
   },
 })
 
+const headerItems: Array<HeaderLinkProps> = [
+  {
+    url: "/community/citation",
+    icon: <AddIcon fontSize="large" />,
+    text: "Cite Us",
+    isNextLink: true,
+  },
+  {
+    url: "/downloads",
+    icon: <FileDownloadIcon fontSize="large" />,
+    text: "Downloads",
+    isNextLink: true,
+  },
+  {
+    url: "/about",
+    icon: <InfoIcon fontSize="large" />,
+    text: "About dictyBase",
+    isNextLink: true,
+  },
+  {
+    url: "/login",
+    icon: <LoginIcon fontSize="large" />,
+    text: "Login",
+    isNextLink: true,
+  },
+]
+
 export default function ComponentDocsApp({ Component, pageProps }: AppProps) {
   return (
     <ClientOnly>
@@ -77,7 +112,21 @@ export default function ComponentDocsApp({ Component, pageProps }: AppProps) {
 
           <title>dictyBase</title>
         </Head>
+        <Header logo={logo.src} caption="Dicty Community Resource">
+          {headerItems.map((item, i) => (
+            <HeaderLink key={i} {...item} />
+          ))}
+        </Header>
+        <Navbar
+          dataUrl="https://raw.githubusercontent.com/dictyBase/migration-data/master/navbar/navbar.json"
+          initialData={navbarData}
+        />
         <Component {...pageProps} />
+        <Footer title="Dicty Community Resource">
+          {footerData.map(({ label, url }, i) => (
+            <FooterLink label={label} url={url} key={i} />
+          ))}
+        </Footer>
       </MuiThemeProvider>
     </ClientOnly>
   )
