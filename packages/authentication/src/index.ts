9c602477f416a65a06c9272dedd37bbfb7e5dd86
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-03-02 09:21:15 -0600

    feat(authentication): add util methods

--- packages/authentication/src/index.ts
+++ packages/authentication/src/index.ts
@@ -1,2 +1,3 @@
 export * from "@dictyBase/authentication/src/types"
 export * from "@dictyBase/authentication/src/config/oauthConfig"
+export * from "@dictyBase/authentication/src/utils"
---
+++ packages/authentication/src/utils.ts
@@ -0,0 +1,17 @@
+import { IOauthProviderConfig } from "./types/index"
+
+const generateParamString = (params: Array<Array<string>>) => {
+  return params
+    .reduce((prev, [key, val]) => [...prev, `${key}=${val}`], [])
+    .join("&")
+}
+
+export const createOauthUrl = (config: IOauthProviderConfig) => {
+  let url = `${config.authorizationEndpoint}?client_id=${config.clientId}`
+  url += `&scope=${config.scopes.join(config.scopeDelimiter)}`
+  if (config.requiredUrlParams) {
+    url += generateParamString(config.requiredUrlParams)
+  }
+  url += `&redirect_uri=${config.redirectUrl}`
+  return url
+}
