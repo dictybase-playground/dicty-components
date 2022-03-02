9c733bedfbdf657c00c4b174ac56dc178e090ada
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-03-02 16:20:34 -0600

    feat(frontend): set event handler

--- apps/frontend/pages/auth/login.tsx
+++ apps/frontend/pages/auth/login.tsx
@@ -1,5 +1,6 @@
 import { Container, Box, Typography } from "@material-ui/core"
-import { openOauthWindow } from "@dictyBase/authentication"
+import { OauthCallbackData, openOauthWindow } from "@dictyBase/authentication"
+import { useEffect, useState } from "react"
 import {
   OrcidButton,
   GoogleButton,
@@ -7,6 +8,28 @@ import {
 } from "@dictyBase/login-buttons"
 
 export default function LoginPage() {
+  const [openOauth, setOpenOauth] = useState(false)
+
+  const oauthWindowCallback = () => {
+    setOpenOauth(true)
+  }
+
+  useEffect(() => {
+    if (!openOauth) return
+
+    // set event listener to pop-up window message
+    const onMessage = (event: MessageEvent) => {
+      event.preventDefault()
+      event.stopPropagation()
+      if (!event.data.provider) return
+
+      const { query } = event.data as OauthCallbackData
+      console.log(query.substring(1).split("&"))
+    }
+    window.addEventListener("message", onMessage, false)
+    return () => window.removeEventListener("message", onMessage)
+  }, [openOauth])
+
   return (
     <Container maxWidth="xs">
       <Box mt={2} mb={3}>
@@ -18,19 +41,19 @@ export default function LoginPage() {
         {/* {error && <ErrorNotification error={message} />} */}
         <Box mb={2} fontWeight={900}>
           <OrcidButton
-            handleClick={() => openOauthWindow("orcid")}
+            handleClick={() => openOauthWindow("orcid", oauthWindowCallback)}
             text="Sign in with ORCID"
           />
         </Box>
         <Box mb={2}>
           <GoogleButton
-            handleClick={() => openOauthWindow("google")}
+            handleClick={() => openOauthWindow("google", oauthWindowCallback)}
             text="Sign in with Google"
           />
         </Box>
         <Box mb={6}>
           <LinkedInButton
-            handleClick={() => openOauthWindow("linkedin")}
+            handleClick={() => openOauthWindow("linkedin", oauthWindowCallback)}
             text="Sign in with LinkedIn"
           />
         </Box>
