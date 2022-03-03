862c05a8db9d92e416d9b2a97fae337a7f56e5ea
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-03-03 17:07:41 -0600

    feat(frontend): use `oauthLoginInput` for use with `useLoginMutation`

--- apps/frontend/pages/auth/[provider]/callback.tsx
+++ apps/frontend/pages/auth/[provider]/callback.tsx
@@ -1,24 +1,28 @@
 import { useRouter } from "next/router"
-import { Provider, OauthCallbackData } from "@dictyBase/authentication"
+import { Provider, oauthLoginInput } from "@dictyBase/authentication"
 import { useEffect } from "react"
 import { Box, Typography } from "@material-ui/core"
 
 export default function OauthCallbackPage() {
   const { query, asPath } = useRouter()
-  const provider = query.provider as Provider
 
   useEffect(() => {
+    // do not proceed until provider and code are defined
+    if (!query.provider || !query.code) return
+
+    const provider = query.provider as Provider
+    const code = query.code as string
+
     // asPath returns the the entire url minus the origin
     // ex. http://example.com/foo/bar?name=brad -> /foo/bar?name=brad
     const queryStart = asPath.indexOf("?")
-    const message: OauthCallbackData = {
-      query: asPath.substring(queryStart),
-      provider: provider,
-      url: `${window.location.origin}${asPath.substring(0, queryStart)}`,
-    }
-    window.opener?.postMessage(message, window.location.toString())
-    window.close()
-  }, [asPath, provider])
+    const origin = window.location.origin
+    // entire url (with the origin) without any query params
+    const url = `${origin}${asPath.substring(0, queryStart)}`
+
+    const input = oauthLoginInput(provider, code, origin, url)
+    console.log(input)
+  }, [asPath, query])
 
   return (
     <Box textAlign="center">
