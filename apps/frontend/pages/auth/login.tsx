e8c18e992783f49228630bfeaec4cad323f28f61
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-03-03 14:06:06 -0600

    feat(authentication): simplify oauth
    remove pop-up window with oauth page, and remove event handlers

--- apps/frontend/pages/auth/login.tsx
+++ apps/frontend/pages/auth/login.tsx
@@ -1,6 +1,6 @@
 import { Container, Box, Typography } from "@material-ui/core"
-import { OauthCallbackData, openOauthWindow } from "@dictyBase/authentication"
-import { useEffect, useState } from "react"
+import { openOauth } from "@dictyBase/authentication"
+import { useRouter } from "next/router"
 import {
   OrcidButton,
   GoogleButton,
@@ -8,27 +8,11 @@ import {
 } from "@dictyBase/login-buttons"
 
 export default function LoginPage() {
-  const [openOauth, setOpenOauth] = useState(false)
+  const router = useRouter()
 
-  const oauthWindowCallback = () => {
-    setOpenOauth(true)
+  const oauthWindowCallback = (oauthUrl: string) => {
+    router.push(oauthUrl)
   }
-
-  useEffect(() => {
-    if (!openOauth) return
-
-    // set event listener to pop-up window message
-    const onMessage = (event: MessageEvent) => {
-      event.preventDefault()
-      event.stopPropagation()
-      if (!event.data.provider) return
-
-      const { query } = event.data as OauthCallbackData
-      console.log(query.substring(1).split("&"))
-    }
-    window.addEventListener("message", onMessage, false)
-    return () => window.removeEventListener("message", onMessage)
-  }, [openOauth])
 
   return (
     <Container maxWidth="xs">
@@ -38,26 +22,24 @@ export default function LoginPage() {
         </Typography>
       </Box>
       <Box textAlign="center">
-        {/* {error && <ErrorNotification error={message} />} */}
         <Box mb={2} fontWeight={900}>
           <OrcidButton
-            handleClick={() => openOauthWindow("orcid", oauthWindowCallback)}
+            handleClick={() => openOauth("orcid", oauthWindowCallback)}
             text="Sign in with ORCID"
           />
         </Box>
         <Box mb={2}>
           <GoogleButton
-            handleClick={() => openOauthWindow("google", oauthWindowCallback)}
+            handleClick={() => openOauth("google", oauthWindowCallback)}
             text="Sign in with Google"
           />
         </Box>
         <Box mb={6}>
           <LinkedInButton
-            handleClick={() => openOauthWindow("linkedin", oauthWindowCallback)}
+            handleClick={() => openOauth("linkedin", oauthWindowCallback)}
             text="Sign in with LinkedIn"
           />
         </Box>
-        {/* <OauthSignHandler /> */}
       </Box>
     </Container>
   )
