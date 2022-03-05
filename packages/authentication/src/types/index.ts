d7618d22babd27c3c67a37cf9f544ae814f34a7b
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-03-05 11:55:31 -0600

    feat(authentication): add types for auth context

--- packages/authentication/src/types/index.ts
+++ packages/authentication/src/types/index.ts
@@ -1,3 +1,5 @@
+import React from "react"
+import { User } from "dicty-graphql-schema"
 export interface ClientProvider {
   clientId: string
 }
@@ -32,4 +34,50 @@ export interface OauthCallbackData {
   query: string
   provider: Provider
   url: string
+}
+
+/** Auth provider */
+export enum AuthActionType {
+  LOGIN = "LOGIN",
+  LOGIN_ERROR = "LOGIN_ERROR",
+  LOGOUT = "LOGOUT",
+  UPDATE_TOKEN = "UPDATE_TOKEN",
+}
+
+export interface AuthState {
+  isAuthenticated: boolean
+  token: string
+  user: User
+  provider: string
+  error: any
+}
+
+export interface AuthPayload {
+  token: string
+  user: User
+  provider: string
+}
+
+export interface AuthErrorPayload {
+  error: any
+}
+
+export type AuthAction =
+  | {
+      type: AuthActionType.LOGIN
+      payload: AuthPayload
+    }
+  | {
+      type: AuthActionType.LOGIN_ERROR
+      payload: AuthErrorPayload
+    }
+  | { type: AuthActionType.LOGOUT }
+  | {
+      type: AuthActionType.UPDATE_TOKEN
+      payload: AuthPayload
+    }
+
+export interface AuthStateContextProps {
+  state: AuthState
+  dispatch: React.Dispatch<AuthAction>
 }
