95333022db5294bbe0ce9b83c0e9e36a6cac6d56
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-03-09 11:43:07 -0600

    build(frontend): add deps

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -3,6 +3,7 @@ const withTM = require("next-transpile-modules")([
   "@dictyBase/header",
   "@dictyBase/footer",
   "@dictyBase/login-buttons",
+  "@dictyBase/provider",
 ])
 
 module.exports = withTM({
