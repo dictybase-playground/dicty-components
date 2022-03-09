d10230a3cd5db14f9402a5db8b7ef48c3bdefab8
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-03-09 16:18:56 -0600

    feat(frontend): call login mutation

--- apps/frontend/pages/auth/[provider]/callback.tsx
+++ apps/frontend/pages/auth/[provider]/callback.tsx
@@ -2,20 +2,30 @@ import { useRouter } from "next/router"
 import { Provider, oauthLoginInput } from "@dictyBase/authentication"
 import { useEffect } from "react"
 import { Box, Typography } from "@material-ui/core"
+import { useLoginMutation } from "dicty-graphql-schema"
 
 export default function OauthCallbackPage() {
   const { query } = useRouter()
+  const [login] = useLoginMutation()
 
   useEffect(() => {
     // do not proceed until provider and code are defined
     if (!query.provider || !query.code) return
 
-    const provider = query.provider as Provider
-    const code = query.code as string
+    const auth = async () => {
+      const provider = query.provider as Provider
+      const code = query.code as string
 
-    const input = oauthLoginInput(provider, code)
-    console.log(input)
-  }, [query])
+      const input = oauthLoginInput(provider, code)
+      try {
+        const { data } = await login({ variables: { input } })
+        console.log(data)
+      } catch (err) {
+        console.error("Could not process callback credentials", err)
+      }
+    }
+    auth()
+  }, [query, login])
 
   return (
     <Box textAlign="center">
