a3feb217b49de677c9f7c0987866d159c12053b2
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-03-09 12:49:44 -0600

    refactor(frontend): remove redundant client render component

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { CssBaseline, Container } from "@material-ui/core"
 import { AppProps } from "next/app"
 import Head from "next/head"
-import ClientOnly from "../components/clientOnly"
 import { Header, HeaderLink } from "@dictyBase/header"
 import logo from "../public/logo.png"
 import { Navbar, navbarData } from "@dictyBase/navbar"
@@ -13,49 +12,47 @@ import { AuthProvider } from "@dictyBase/authentication"
 export default function ComponentDocsApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <ClientOnly>
-        <AppProvider
-          server={process.env.NEXT_PUBLIC_GRAPHQL_SERVER}
-          altServer={process.env.NEXT_PUBLIC_ALT_GRAPHQL_SERVER}
-          deployEnv={process.env.NEXT_PUBLIC_DEPLOY_ENV}>
-          <CssBaseline />
-          <Head>
-            <link rel="shortcut icon" href="favicon.ico" />
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1, shrink-to-fit=no"
-            />
-            <meta name="theme-color" content="#000000" />
-
-            <link rel="manifest" href="manifest.json" />
-
-            <title>dictyBase</title>
-          </Head>
-
-          <Header logo={logo.src} caption="Dicty Community Resource">
-            {headerLoginData.map((item, i) => (
-              <HeaderLink key={i} {...item} />
-            ))}
-          </Header>
-          <Navbar
-            dataUrl="https://raw.githubusercontent.com/dictyBase/migration-data/master/navbar/navbar.json"
-            initialData={navbarData}
+      <AppProvider
+        server={process.env.NEXT_PUBLIC_GRAPHQL_SERVER}
+        altServer={process.env.NEXT_PUBLIC_ALT_GRAPHQL_SERVER}
+        deployEnv={process.env.NEXT_PUBLIC_DEPLOY_ENV}>
+        <CssBaseline />
+        <Head>
+          <link rel="shortcut icon" href="favicon.ico" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
+          <meta name="theme-color" content="#000000" />
 
-          {/* render pages here */}
-          <Container maxWidth="lg">
-            <br />
-            <Component {...pageProps} />
-            <br />
-          </Container>
+          <link rel="manifest" href="manifest.json" />
 
-          <Footer title="Dicty Community Resource">
-            {footerData.map(({ label, url }, i) => (
-              <FooterLink label={label} url={url} key={i} />
-            ))}
-          </Footer>
-        </AppProvider>
-      </ClientOnly>
+          <title>dictyBase</title>
+        </Head>
+
+        <Header logo={logo.src} caption="Dicty Community Resource">
+          {headerLoginData.map((item, i) => (
+            <HeaderLink key={i} {...item} />
+          ))}
+        </Header>
+        <Navbar
+          dataUrl="https://raw.githubusercontent.com/dictyBase/migration-data/master/navbar/navbar.json"
+          initialData={navbarData}
+        />
+
+        {/* render pages here */}
+        <Container maxWidth="lg">
+          <br />
+          <Component {...pageProps} />
+          <br />
+        </Container>
+
+        <Footer title="Dicty Community Resource">
+          {footerData.map(({ label, url }, i) => (
+            <FooterLink label={label} url={url} key={i} />
+          ))}
+        </Footer>
+      </AppProvider>
     </AuthProvider>
   )
 }
