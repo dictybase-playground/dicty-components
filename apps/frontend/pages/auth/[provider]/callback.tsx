beb050e58b9ef1c6b6e8c6bbdd4eb791f0bf7c41
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-03-10 12:08:22 -0600

    feat(frontend): update to use new `Callback` props

--- apps/frontend/pages/auth/[provider]/callback.tsx
+++ apps/frontend/pages/auth/[provider]/callback.tsx
@@ -1,17 +1,14 @@
 import { useRouter } from "next/router"
-import { Provider, useAuthStore, Callback } from "@dictyBase/authentication"
+import { Provider, Callback } from "@dictyBase/authentication"
 
 export default function OauthCallbackPage() {
   const { query, push } = useRouter()
-  const { state, dispatch } = useAuthStore()
 
   return (
     <Callback
       provider={query.provider as Provider}
       code={query.code as string}
-      state={state}
-      dispatch={dispatch}
-      callback={() => push("/")}
+      callback={(_) => push("/")}
     />
   )
 }
