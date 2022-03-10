bdea3084ab03f4a30748604ce7f64036f71ad26b
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-03-09 21:11:05 -0600

    feat(frontend): mock login mutation

--- apps/frontend/mocks/handlers.ts
+++ apps/frontend/mocks/handlers.ts
@@ -1,12 +1,14 @@
 import { graphql } from "msw"
-import { LoginInput } from "dicty-graphql-schema"
+import { LoginInput, Auth, Identity, User } from "dicty-graphql-schema"
 
 export const handlers = [
-  // Handles the Gene query: https://github.com/dictyBase/dicty-graphql-schema/blob/develop/src/queries/gene.graphql
-  // Implementation details here: https://github.com/dictyBase/genomepage/pull/825#issuecomment-977246804
-  graphql.query("Login", (req, res, ctx) => {
+  graphql.mutation("Login", (req, res, ctx) => {
     const variables = req.body.variables as { input: LoginInput }
-    console.log(variables.input)
-    res(ctx.data({}))
+    const auth: Auth = {
+      identity: { provider: "google" } as Identity,
+      token: "my-token",
+      user: {} as User,
+    } as Auth
+    return res(ctx.data({ login: { ...auth } }))
   }),
 ]
