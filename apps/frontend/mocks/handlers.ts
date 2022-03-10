258026737a8457eda228fd986b7b69f921a2542d
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-03-09 22:01:06 -0600

    feat(frontend): return dummy auth data

--- apps/frontend/mocks/handlers.ts
+++ apps/frontend/mocks/handlers.ts
@@ -6,8 +6,14 @@ export const handlers = [
     const variables = req.body.variables as { input: LoginInput }
     const auth: Auth = {
       identity: { provider: "google" } as Identity,
-      token: "my-token",
-      user: {} as User,
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+      user: {
+        city: "Chicago",
+        first_name: "John",
+        last_name: "Doe",
+        email: "user@example.com",
+      } as User,
     } as Auth
     return res(ctx.data({ login: { ...auth } }))
   }),
