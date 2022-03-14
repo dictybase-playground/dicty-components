dc7a0f1ffdd015e9ba782d42463b6434c97bfa1d
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-03-14 17:39:17 -0500

    test(authentication): experimenting with mock dicty-graphql-schema methods

--- packages/authentication/src/__tests__/Callback.test.tsx
+++ packages/authentication/src/__tests__/Callback.test.tsx
@@ -1,49 +1,88 @@
 import { ApolloError } from "@apollo/client"
-import { Callback, MockProvider } from "@dictyBase/authentication"
+import {
+  Callback,
+  MockProvider,
+  MockSuperuser,
+} from "@dictyBase/authentication"
 import { render, screen } from "@testing-library/react"
-import { User } from "dicty-graphql-schema"
-
-const mockUser = {
-  id: "1",
-  first_name: "john",
-  last_name: "doe",
-  email: "user@example.com",
-} as User
-
-jest.mock("dicty-graphql-schema", () => {
-  const originalModule = jest.requireActual("dicty-graphql-schema")
-  return {
-    ...originalModule,
-    useLoginMutation: [
-      async (variables: any) => ({
-        data: {
-          login: {
-            token: "my-token",
-            user: mockUser,
-            provider: "google",
-          },
-        },
-      }),
-      { error: ApolloError },
-    ],
-  }
-})
+import { useLoginMutation, LoginMutation } from "dicty-graphql-schema"
 
 describe("OAuth callback", () => {
-  it("should update state", () => {
-    let val = 1
+  beforeEach(() => {
+    jest.resetModules()
+  })
+
+  jest.mock("dicty-graphql-schema", () => ({
+    useLoginMutation: jest.fn(),
+  }))
+
+  test("show loading ui", () => {
+    jest.doMock("dicty-graphql-schema", () => {
+      return {
+        useLoginMutation: (baseOptions?: any) => [
+          () => {},
+          {
+            data: null,
+            error: undefined,
+            loading: false,
+          },
+        ],
+      }
+    })
+
     render(
       <MockProvider>
-        <Callback
-          provider={"google"}
-          code={"123"}
-          callback={(state) => {
-            val++
-          }}
-        />
+        <Callback provider={"google"} code={"123"} />
       </MockProvider>,
     )
     expect(screen.getByText("Logging in")).toBeDefined()
-    expect(val).toBe(2)
   })
 })
+
+// test("show data ui", () => {
+//   jest.doMock("dicty-graphql-schema", () => ({
+//     useLoginMutation: (baseOptions?: any) => [
+//       async (options?: any) => ({}),
+//       {
+//         error: undefined,
+//         data: {
+//           __typename: "Mutation",
+//           login: {
+//             __typename: "Auth",
+//             token: "235toijeorg8792",
+//             user: MockSuperuser,
+//             identity: { __typename: "Identity", provider: "google" },
+//           },
+//         } as LoginMutation,
+//         loading: false,
+//       },
+//     ],
+//   }))
+
+//   render(
+//     <MockProvider>
+//       <Callback provider={"google"} code={"123"} />
+//     </MockProvider>,
+//   )
+//   expect(screen.getByText("Redirecting")).toBeDefined()
+// })
+
+// test("show error ui", () => {
+//   jest.doMock("dicty-graphql-schema", () => ({
+//     useLoginMutation: (baseOptions?: any) => [
+//       async (options?: any) => ({}),
+//       {
+//         error: new ApolloError({}),
+//         data: undefined,
+//         loading: false,
+//       },
+//     ],
+//   }))
+
+//   render(
+//     <MockProvider validToken={false}>
+//       <Callback provider={"google"} code={"123"} />
+//     </MockProvider>,
+//   )
+//   expect(screen.getByText("Could not login")).toBeDefined()
+// })
